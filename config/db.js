a764cd92cc6ae027bacb6dae8034158fb804dd10
Brighton <BrightonDube@users.noreply.github.com>
2025-04-06 23:43:01 +0200

    `Added dependencies to package.json`

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const mongoUri = process.env.MONGO_URI; 
+        await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1); 
+    }
+};
+
+module.exports = connectDB;
