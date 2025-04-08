511e079569dc129903699d321e0f419880217bfa
Brighton <BrightonDube@users.noreply.github.com>
2025-04-08 22:57:18 +0200

    `Removed secure option from cookie configuration in server.js`

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      // secure: process.env.NODE_ENV === 'production',
       maxAge: 1000 * 60 * 60 * 24,
     },
   })
